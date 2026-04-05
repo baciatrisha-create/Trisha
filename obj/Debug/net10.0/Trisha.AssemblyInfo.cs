46fdf8fa12976a700ba60ec0ecfef208ce6b66d7
baciatrisha-create <baciatrisha@gmail.com>
2026-04-05 22:58:19 +0800

    5th commit

--- obj/Debug/net10.0/Trisha.AssemblyInfo.cs
+++ obj/Debug/net10.0/Trisha.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trisha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11d76a2c8636d61b7ed6490ac5e58e8fc3c4da96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ffd2ab74b791bd74924bf8c960fe2215a0fa646")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trisha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trisha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
